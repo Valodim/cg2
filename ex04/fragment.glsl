5265085d6b6bc65c9e15a1d599b065ac5e634dea
Martin Wegner <mw@mroot.net>
2011-05-15 22:14:33 +0200

    ex04: First shot at vertex and fragment shader programs.
    Seriously f*cked up, though, at the moment ...

--- ex04/fragment.glsl
+++ ex04/fragment.glsl
@@ -1,3 +1,20 @@
-main() {
-	gl_FragColor = gl_Color;
+varying vec3 normal, lightDirection, eyeDirection;
+uniform float innerSpotAngle, outerSpotAngle;
+
+void main() {
+
+	vec3 lightDirectionNorm = normalize(lightDirection);
+	vec3 eyeDirectionNorm = normalize(lightDirection);
+
+	float angle = acos(dot(lightDirectionNorm, eyeDirectionNorm));
+
+	if(angle <= innerSpotAngle) {
+		gl_FragColor.rgb = vec3(1.0, 1.0, 1.0);
+	}
+	else if(angle <= outerSpotAngle) {
+		gl_FragColor.rgb = vec3(1.0, 1.0, 1.0) * (1 - ((angle - innerSpotAngle) / (outerSpotAngle - innerSpotAngle)));
+	}
+	else {
+		gl_FragColor.rgb = vec3(0.2, 0.2, 0.2);
+	}
 }
--- ex04/vertex.glsl
+++ ex04/vertex.glsl
@@ -1,3 +1,10 @@
-main() {
-	gl_position = ftransform();
+varying vec3 normal, lightDirection, eyeDirection;
+
+void main() {
+	gl_Position = ftransform();
+
+	normal = gl_NormalMatrix * gl_Normal;
+	vec3 pos = (gl_ModelViewMatrix * gl_Vertex).xyz;
+	lightDirection = gl_LightSource[0].position.xyz - pos;
+	eyeDirection = -pos;
 }
