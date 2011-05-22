c9565bc92eba74da20aa7e5077d3c8b648c105d5
Valodim <valodim@mugenguild.com>
2011-05-22 13:52:19 +0200

    ex04: fix shaders, working properly now

--- ex04/shaders/fragment.s
+++ ex04/shaders/fragment.s
@@ -1,7 +1,7 @@
 #version 120
 
-varying vec3 vert_norm_dir;
 varying vec3 vert_light_dir;
+varying vec3 vert_norm_dir;
 
 uniform float uni_outerSpotAngle;
 uniform float uni_innerSpotAngle;
@@ -11,16 +11,19 @@ void main () {
     // normalize everything necessary //
     vec3 N = normalize(vert_norm_dir);
     vec3 L = normalize(vert_light_dir);
+    vec3 X = vec3(0, 0, 1);
 
     // diffuse component //
-    float NdotL = max(0.0, dot(N, L));
-    if(NdotL < 0.1f)
+    float angle = acos(dot(X, L));
+    if(angle < uni_innerSpotAngle)
         gl_FragColor = vec4(0.5, 0.5, 0.0, 1.0);
-    else if(NdotL < uni_outerSpotAngle)
-        gl_FragColor = vec4(0.0, 0.5, 0.5, 1.0) * NdotL;
+    else if(angle < uni_outerSpotAngle)
+        gl_FragColor = vec4(0.0, 0.5, 0.5, 1.0) * (1 - (angle - uni_innerSpotAngle) / (uni_outerSpotAngle - uni_innerSpotAngle));
     else
         gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
 
     gl_FragColor += vec4(0.2, 0.2, 0.2, 0.0);
+
+    gl_FragColor *= dot(N, L);
 
 }
